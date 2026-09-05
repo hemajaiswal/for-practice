e5597984b224c13a9a4a9b771897f172aea89e36
HEMA JAISWAL <hemajaiswal@HEMAs-MacBook-Air.local>
2026-09-05 13:33:26 +0530

    Add Week 1 practical

---
+++ week1/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+  function App() {
+    return (
+      <div>
+        <h1>Welcome to React</h1>
+        <p>My First React Application</p>
+      </div>
+    );
+  }
+export default App;
